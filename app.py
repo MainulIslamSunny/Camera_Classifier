5067e435b5ee28e7c3dae09cbd772fdc1b41cac6
Mainul Islam <s.m.sunny97@gmail.com>
2023-08-29 20:19:33 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,179 @@
+import tkinter as tk
+from tkinter import simpledialog
+import cv2 as cv
+import os
+import PIL.Image, PIL.ImageTk
+import model
+import camera
+import numpy as np
+
+class App:
+
+    def __init__(self, window=tk.Tk(), window_title="Camera Classifier"):
+
+        self.window = window
+        self.window_title = window_title
+
+        self.counters = [1, 1]
+
+        self.model = model.Model()
+
+        self.auto_predict = False
+
+        self.camera = camera.Camera()
+
+        self.init_gui()
+
+        self.delay = 15
+        self.update()
+
+        self.window.attributes("-topmost", True)
+        self.window.mainloop()
+
+    def init_gui(self):
+
+        self.canvas = tk.Canvas(self.window, width=self.camera.width, height=self.camera.height)
+        self.canvas.pack()
+
+        self.btn_toggleauto = tk.Button(self.window, text="Auto Prediction", width=50, command=self.auto_predict_toggle)
+        self.btn_toggleauto.pack(anchor=tk.CENTER, expand=True)
+
+        self.classname_one = simpledialog.askstring("Classname One", "Enter the name of the first class:", parent=self.window)
+        self.classname_two = simpledialog.askstring("Classname Two", "Enter the name of the second class:", parent=self.window)
+
+        self.btn_class_one = tk.Button(self.window, text=self.classname_one, width=50, command=lambda: self.save_for_class(1))
+        self.btn_class_one.pack(anchor=tk.CENTER, expand=True)
+
+        self.btn_class_two = tk.Button(self.window, text=self.classname_two, width=50, command=lambda: self.save_for_class(2))
+        self.btn_class_two.pack(anchor=tk.CENTER, expand=True)
+
+        self.btn_train = tk.Button(self.window, text="Train Model", width=50, command=lambda: self.model.train_model(self.counters))
+        self.btn_train.pack(anchor=tk.CENTER, expand=True)
+
+        self.btn_predict = tk.Button(self.window, text="Predict", width=50, command=self.predict)
+        self.btn_predict.pack(anchor=tk.CENTER, expand=True)
+
+        self.btn_reset = tk.Button(self.window, text="Reset", width=50, command=self.reset)
+        self.btn_reset.pack(anchor=tk.CENTER, expand=True)
+
+        self.class_label = tk.Label(self.window, text="CLASS")
+        self.class_label.config(font=("Arial", 20))
+        self.class_label.pack(anchor=tk.CENTER, expand=True)
+
+
+    def auto_predict_toggle(self):
+        self.auto_predict = not self.auto_predict
+
+        if self.auto_predict:
+            self.predict_from_camera()
+
+    def save_for_class(self, class_num):
+        ret, frame = self.camera.get_frame()
+        if not os.path.exists("1"):
+            os.mkdir("1")
+        if not os.path.exists("2"):
+            os.mkdir("2")
+
+        cv.imwrite(f'{class_num}/frame{self.counters[class_num-1]}.jpg', cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
+        img = PIL.Image.open(f'{class_num}/frame{self.counters[class_num - 1]}.jpg')
+        img.thumbnail((150, 150), PIL.Image.ANTIALIAS)
+        img.save(f'{class_num}/frame{self.counters[class_num - 1]}.jpg')
+
+        self.counters[class_num - 1] += 1
+
+    def reset(self):
+        for folder in ['1', '2']:
+            for file in os.listdir(folder):
+                file_path = os.path.join(folder, file)
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+
+        self.counters = [1, 1]
+        self.model = model.Model()
+        self.class_label.config(text="CLASS")
+
+    def update(self):
+        ret, frame = self.camera.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+
+        self.window.after(self.delay, self.update)
+
+    def predict(self):
+        frame = self.camera.get_frame()
+        prediction = self.model.predict(frame)
+
+        if prediction == 1:
+            self.class_label.config(text=self.classname_one)
+            return self.classname_one
+        if prediction == 2:
+            self.class_label.config(text=self.classname_two)
+            return self.classname_two
+
+    def predict_from_camera(self):
+        net = cv.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+
+        classes = []
+        with open("coco.names", "r") as f:
+            classes = f.read().splitlines()
+
+        cap = cv.VideoCapture(0)
+
+        while self.auto_predict:
+            _, img = cap.read()
+            height, width, _ = img.shape
+
+            blob = cv.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
+
+            net.setInput(blob)
+            output_layers_names = net.getUnconnectedOutLayersNames()
+            layerOutputs = net.forward(output_layers_names)
+
+            boxes = []
+            confidences = []
+            class_ids = []
+
+            for output in layerOutputs:
+                for detection in output:
+                    scores = detection[5:]
+                    class_id = np.argmax(scores)
+                    confidence = scores[class_id]
+                    if confidence > 0.5:
+                        center_x = int(detection[0]*width)
+                        center_y = int(detection[1]*height)
+                        w = int(detection[2]*width)
+                        h = int(detection[3]*height)
+
+                        x = int(center_x - w/2)
+                        y = int(center_y - h/2)
+
+                        boxes.append([x, y, w, h])
+                        confidences.append((float(confidence)))
+                        class_ids.append(class_id)
+
+            indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+            font = cv.FONT_HERSHEY_PLAIN
+            colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+
+            for i in indexes.flatten():
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                confidence = str(round(confidences[i], 2))
+                color = colors[i]
+                cv.rectangle(img, (x,y), (x+w, y+h), color, 2)
+                cv.putText(img, label + " " + confidence, (x, y+20), font, 2, (255, 255, 255), 2)
+
+            self.class_label.config(text=label)
+
+            cv.imshow('Image', img)
+            key = cv.waitKey(1)
+            if key == 27:
+                break
+
+        cap.release()
+        cv.destroyAllWindows()
+
+app = App()
